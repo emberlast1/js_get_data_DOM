1c4f86714f83276ab1f43ddf690d440792c275f2
Ostap Hedz <lordanvil18@gmail.com>
2024-07-18 13:36:32 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const populationArr = [
+    ...document.querySelectorAll(".population")
+].map((element)=>+element.innerText.split(",").join(""));
+const totalPopulation = populationArr.reduce((sum, population)=>sum + population, 0);
+const avaragePopulation = totalPopulation / populationArr.length;
+document.querySelector(".total-population").textContent = totalPopulation.toLocaleString("en-US");
+document.querySelector(".average-population").textContent = avaragePopulation.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
